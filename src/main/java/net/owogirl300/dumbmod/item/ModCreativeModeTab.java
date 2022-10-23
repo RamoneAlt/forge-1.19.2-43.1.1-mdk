e057834cfab1cab02e6e674b7dcde72357af86db
test <ramonealt@gmail.com>
2022-10-23 15:46:08 -0400

    new creative mode tab containing zircon/raw_zircon

---
+++ src/main/java/net/owogirl300/dumbmod/item/ModCreativeModeTab.java
@@ -0,0 +1,13 @@
+package net.owogirl300.dumbmod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class ModCreativeModeTab {
+    public static final CreativeModeTab DUMB_TAB = new CreativeModeTab("dumbtab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(ModItems.ZIRCON.get());
+        }
+    };
+}
--- src/main/java/net/owogirl300/dumbmod/item/ModItems.java
+++ src/main/java/net/owogirl300/dumbmod/item/ModItems.java
@@ -1,6 +1,6 @@
 package net.owogirl300.dumbmod.item;
 
-import net.minecraft.world.item.CreativeModeTab;
+
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,10 +13,10 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, DumbMod.MODID);
 
     public static final RegistryObject<Item> ZIRCON = ITEMS.register( "zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DUMB_TAB)));
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register( "raw_zircon",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.DUMB_TAB)));
 
     public static void register (IEventBus eventBus) {
         ITEMS.register(eventBus);
