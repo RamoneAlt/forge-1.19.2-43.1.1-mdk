ebde44ed9d8e8acaa387d2f4d80d581b23f5bbaa
Ramone <116516798+RamoneAlt@users.noreply.github.com>
2023-03-17 22:22:55 -0400

    added marble stairs / slabs

--- src/main/java/net/owogirl300/dumbmod/painting/ModPaintings.java
+++ src/main/java/net/owogirl300/dumbmod/painting/ModPaintings.java
@@ -9,10 +9,12 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ModPaintings {
     public static final DeferredRegister<PaintingVariant> PAINTING_VARIANTS =
-            DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, DumbMod.MODID);
+            DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS,DumbMod.MODID);
 
     public static final RegistryObject<PaintingVariant> AMOGUS = PAINTING_VARIANTS.register("amogus",
             () -> new PaintingVariant(16, 16));
+    public static final RegistryObject<PaintingVariant> COCK = PAINTING_VARIANTS.register("cock",
+            () -> new PaintingVariant(16, 32));
     public static void register(IEventBus eventBus) {
         PAINTING_VARIANTS.register(eventBus);
     }
